b7e450fa79fcdd46deb3352488c90c1603fd60df
rerost <gmailii@yahoo.co.jp>
2018-05-28 02:06:38 +0900

    Add mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "xss_rspec"
+require "timecop"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -10,5 +11,42 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:each) do
+    class MockColumn
+      def initialize(type: nil, name: nil)
+        @_type = type
+        @_name = name
+      end
+      def type
+        return @_type
+      end
+      def name
+        return @_name
+      end
+    end
+    class MockUser
+      def self.columns_hash
+        {
+          id: MockColumn.new(type: :integer, name: :id),
+          name: MockColumn.new(type: :string, name: :name),
+          created_at: MockColumn.new(type: :datetime, name: :created_at),
+        }
+      end
+
+      def self.columns
+        self.columns_hash.map { |k, _| 
+          MockColumn.new(name: k.to_sym)
+        }
+      end
+    end
+    class MockConnection
+      def tables
+        ["mock_users"]
+      end
+    end
+    allow(ActiveRecord::Base).to receive(:connection) { MockConnection.new }
+    Timecop.freeze
   end
 end
